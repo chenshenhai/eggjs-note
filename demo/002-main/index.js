4fb3c42943a8bb38cfe74b06d5aa1822e7c68991
chenshenhai <cshenhai@gmail.com>
2018-01-07 20:57:55 +0800

    feat: update demo 002-main

--- demo/002-main/index.js
+++ demo/002-main/index.js
@@ -5,5 +5,19 @@ const app = new Application({
   type: 'application',
 });
 
-app.listen(7001);
-console.log('server started at 7001');
+ 
+app.ready(( err )=>{
+  if (err) { 
+    console.log(err);
+    process.exit(1);
+  }
+  let server = require('http').createServer(app.callback());
+  server.once('error', err => {
+    console.log('[app_worker] server got error: %s, code: %s', err.message, err.code);
+    process.exit(1);
+  });
+  server.listen(7001, () => {
+    console.log('server started at 7001');
+  })
+})
+
